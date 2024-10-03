c23a3deee6dfd8d6749b5bc19b0143e890025cf2
Frederic <fredericdonfack13@gmail.com>
2024-10-03 17:07:24 +0100

    fin du projet

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,18 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-
+  isOpen : boolean = false;
+  toggleSideBar(){
+    this.isOpen = !this.isOpen;
+  }
 }
