7c342a4e8956c44b50a4b799e75902ee97981029
Salam Sahane <salam.sahane@londo-tech.com>
2024-10-09 11:36:11 +0100

    refactor(header.component): add routerLink for navigation, convert 'isOpen' to a signal, refactor toggleSidebar as arrow function

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,19 @@
 import { NgClass } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    NgClass
+    NgClass,
+    RouterLink
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-  isOpen : boolean = false;
-  toggleSideBar(){
-    this.isOpen = !this.isOpen;
-  }
+  isOpen: WritableSignal<boolean> = signal(false);
+
+  toggleSideBar = () => this.isOpen.set(!this.isOpen());
 }
